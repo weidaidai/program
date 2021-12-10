2302abff51e8c7b2e213eca30f3054bbda38a949
weidongqi <591269693@qq.com>
2021-12-10 15:22:13 +0800

    修改mysql 全局变量，修改为传参，错误处理打印改为log

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql" //database/sql仅提供基本的接口，还需指定一个第三方的数据库
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,47 +15,34 @@ type user struct {
 	name string //姓名
 }
 
-// 定义一个全局对象db
-var db *sql.DB
-
 // 定义一个初始化数据库的函数
-func initDB() (err error) {
+func initDB(dsn string) (db *sql.DB, err error) {
 
-	dsn := "root:123456@tcp(127.0.0.1:3306)/sql_domo"
 	//初始化全局的db对象
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		return err
-	}
-	// 尝试与数据库建立连接（校验dsn是否正确）
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("连接MYSQL失败", err)
-		return err
-	}
-
-	return nil
-}
-
-//单行查询
-func queryrow() {
-	single_data := "select id,name,age from user where id=?"
-	var u user
-	err := db.QueryRow(single_data, 0).Scan(&u.id, &u.name, &u.age)
-	if err != nil {
-		fmt.Println("查询失败", err)
+		log.Fatal(err)
 		return
-	}
-	fmt.Printf("id:%d name:%s age:%d", u.id, u.name, u.age)
 
+	}
+	//设置数据库最大连接数
+	db.SetConnMaxLifetime(10)
+	//设置上数据库最大闲置连接数
+	db.SetMaxIdleConns(5)
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	return db, err
 }
 
-// 查询多条数据
-func queryMultiRow() {
-	sql_syntax := "select id, name, age from user where id > ?"
-	rows, err := db.Query(sql_syntax, 0)
+// 查询数据
+func queryMultiRow(db *sql.DB) {
+	sql_syntax := "select id, name, age from user where id> ? and id< ?"
+	rows, err := db.Query(sql_syntax, 1, 3)
 	if err != nil {
-		fmt.Println("查询多条数据失败", err)
+		log.Fatal("查询多条数据失败", err)
 		return
 	}
 	// 关闭rows释放持有的数据库链接
@@ -65,7 +53,7 @@ func queryMultiRow() {
 		var u user
 		err := rows.Scan(&u.id, &u.name, &u.age)
 		if err != nil {
-			fmt.Printf("scan failed, err:%v\n", err)
+			log.Fatal("查询多条数据失败", err)
 			return
 		}
 		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
@@ -73,16 +61,16 @@ func queryMultiRow() {
 }
 
 //插入
-func insertrow() {
+func insertrow(db *sql.DB) {
 	sql_syntax := "insert into user(name,age)values(?,?)"
 	result, err := db.Exec(sql_syntax, "小微", 22)
 	if err != nil {
-		fmt.Println("插入数据失败", err)
+		log.Fatal("插入数据失败", err)
 		return
 	}
 	newid, err := result.LastInsertId()
 	if err != nil {
-		fmt.Println("获取插入的id失败", err)
+		log.Fatal("获取插入的id失败", err)
 		return
 	}
 	fmt.Printf("新插入的id为%d\n", newid)
@@ -90,11 +78,11 @@ func insertrow() {
 }
 
 //更新
-func updaterow() {
+func updaterow(db *sql.DB, err error) {
 	sql_syntax := "update user set name=? where id=?"
 	result, err := db.Exec(sql_syntax, "小可爱", 2)
 	if err != nil {
-		fmt.Println("插入数据失败", err)
+		log.Fatal("插入数据失败", err)
 		return
 	}
 	var n int64
@@ -108,35 +96,34 @@ func updaterow() {
 }
 
 //删除
-func deleterow() {
+func deleterow(db *sql.DB) {
 	sql_syntax := "delete from user where id=?"
 	result, err := db.Exec(sql_syntax, 1)
 	if err != nil {
-		fmt.Println("删除失败", err)
+		log.Fatal("删除失败", err)
 		return
 	}
 
 	delid, err := result.RowsAffected()
 	if err != nil {
-		fmt.Println("删除失败", err)
+		log.Fatal("删除失败", err)
 		return
 	}
 	fmt.Printf("删除id在：%d\n", delid)
 
 }
 func main() {
-	if err := initDB(); err != nil {
-		fmt.Println("初始化连接失败", err)
-		return
+	db, err := initDB("root:123456@tcp(127.0.0.1:3306)/sql_domo")
+	if err != nil {
+		panic(err)
 	}
 	fmt.Println("连接成功")
 	//释放资源
 	defer db.Close()
-	initDB()
-	//queryrow()
-	queryMultiRow()
-	//insertrow()
-	updaterow()
+
+	queryMultiRow(db)
+	//insertrow(db)
+	//updaterow()
 	//deleterow()
 
 }
